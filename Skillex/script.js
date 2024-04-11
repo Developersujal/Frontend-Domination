5e5946a47d73a5a5f6174b1570632d690554f146
Developersujal <159018552+Developersujal@users.noreply.github.com>
2024-04-12 01:18:53 +0530

    skillex page2 added

--- Skillex/script.js
+++ Skillex/script.js
@@ -1,3 +1,5 @@
+var tl = gsap.timeline();
+
 function navanime() {
 var btn = document.querySelector("#btn2");
 
@@ -16,7 +18,8 @@ btn.addEventListener("mouseleave",function(){
 })
 }
 
-var left = document.querySelectorAll("#imgs");
+function imageSlide() {
+    var left = document.querySelectorAll("#imgs");
 
 left.forEach(element => {
     element.addEventListener("mouseenter",()=>{
@@ -32,4 +35,46 @@ left.forEach(element => {
         })
     })
 });
+}
+tl.from("nav",{
+    y:-20,
+    duration:1,
+    opacity:0,
+})
+tl.from(".page1-content",{
+    x:-600,
+    duration:1.2,
+    opacity:0,
+})
+
+function inphover(){
+    var inp = document.querySelector(".input");
+    inp.addEventListener("mouseenter",()=>{
+        gsap.to(inp,{
+            boxShadow:"7px 7px 10px grey"
+        })    
+    })
+    inp.addEventListener("mouseleave",()=>{
+        gsap.to(inp,{
+            boxShadow:"5px 5px 10px white"
+        })    
+    })
+}
+
+
+// gsap.from(".page3 h1",{
+//     y:20,
+//     opacity:0,
+//     stagger:0.3,
+//     scrollTrigger:{
+//         trigger:".page3",
+//         scroller:"#main",
+//         // markers:true,
+//         start:"top 50%",
+//         end:"top -10%",
+//         scrub:true
+//     }
+// })
 navanime();
+imageSlide();
+inphover();
